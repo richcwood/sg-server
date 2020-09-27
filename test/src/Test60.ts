60d3aaa46e01cf1b80797ea2171564b5d9c2d60c
Richard Wood <rich@wifunds.com>
2020-09-26 21:00:52 -0600

    lambda ruby test

---
+++ test/src/Test60.ts
@@ -0,0 +1,121 @@
+import * as config from 'config';
+import * as TestBase from './TestBase';
+import { SGUtils } from '../../server/src/shared/SGUtils';
+import { ScriptType, TaskDefTarget } from '../../server/src/shared/Enums';
+import * as _ from 'lodash';
+
+
+const script1 = `
+print 'start\n'
+sleep(5)
+print 'done\n'
+print '@sgo{"route": "ok"}\n'
+`;
+const script1_b64 = SGUtils.btoa(script1);
+
+
+let self: Test60;
+
+
+export default class Test60 extends TestBase.WorkflowTestBase {
+
+    constructor(testSetup) {
+        super('Test60', testSetup);
+        this.description = 'Run ruby task as lambda';
+
+        self = this;
+    }
+
+
+    public async RunTest() {
+        let result: boolean;
+        let resApiCall: any;
+
+        const _teamId: string = config.get('sgTestTeam');
+
+        const sgAdminTeam: string = config.get('sgAdminTeam');
+        const awsLambdaRequiredTags: string = config.get('awsLambdaRequiredTags');
+        const newAgentProperties = { '_teamId': sgAdminTeam, 'machineId': 'AdminAgent', 'ipAddress': '10.10.0.104', 'tags': awsLambdaRequiredTags, 'numActiveTasks': 0, 'lastHeartbeatTime': null, 'rmqPassword': 'lpUs8Cnsju' };
+        const newAgent = await self.testSetup.InitAgent(newAgentProperties);
+
+        const properties: any = {
+            scripts: [
+                {
+                    name: 'Script 60',
+                    scriptType: ScriptType.RUBY,
+                    code: script1_b64,
+                    shadowCopyCode: script1_b64
+                }
+            ],
+            jobDefs: [
+                {
+                    name: 'Job 60',
+                    taskDefs: [
+                        {
+                            name: 'Task 1',
+                            target: TaskDefTarget.AWS_LAMBDA,
+                            stepDefs: [
+                                {
+                                    name: 'Step 1',
+                                    scriptName: 'Script 60',
+                                    lambdaRuntime: 'ruby2.5',
+                                    lambdaRole: config.get('lambda-admin-iam-role'),
+                                    lambdaAWSRegion: config.get('AWS_REGION'),
+                                    lambdaTimeout: 10
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
+
+        const { scripts, jobDefs } = await this.CreateJobDefsFromTemplates(properties);
+
+        let startedJobId;
+        resApiCall = await this.testSetup.RestAPICall(`job`, 'POST', _teamId, { jobDefName: jobDefs['Job 60'].name });
+        if (resApiCall.data.statusCode != 201) {
+            self.logger.LogError('Failed', { Message: `job POST returned ${resApiCall.data.statusCode}`, _jobDefId: jobDefs['Job 60'].id });
+            return false;
+        }
+        startedJobId = resApiCall.data.data.id;
+
+        const jobStartedBP: any = {
+            domainType: 'Job',
+            operation: 1,
+            model:
+            {
+                _teamId: config.get('sgTestTeam'),
+                _jobDefId: jobDefs[properties.jobDefs[0].name].id,
+                runId: 0,
+                name: properties.jobDefs[0].name,
+                status: 0,
+                id: startedJobId,
+                type: 'Job'
+            }
+        }
+        self.bpMessagesExpected.push(jobStartedBP);
+
+        const jobCompletedBP: any = {
+            domainType: 'Job',
+            operation: 2,
+            model:
+            {
+                status: 20,
+                id: startedJobId,
+                type: 'Job'
+            }
+        };
+        self.bpMessagesExpected.push(jobCompletedBP);
+
+        result = await self.WaitForTestToComplete();
+        if (!result)
+            return result;
+
+        newAgent.offline = true;
+        await newAgent.SendHeartbeat(false, true);
+        await newAgent.Stop();
+    
+        return true;
+    }
+}
