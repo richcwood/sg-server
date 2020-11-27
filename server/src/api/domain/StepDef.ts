b6237c9f8b48ce5387a093f6036b19e8fa0277de
Richard Wood <rich@wifunds.com>
2020-11-27 13:05:17 -0700

    added lambdaCodeSource property to stepdef

--- server/src/api/domain/StepDef.ts
+++ server/src/api/domain/StepDef.ts
@@ -37,6 +37,9 @@ export class StepDefSchema {
   @mapProp({of: String, default: new Map([])})
   variables?: Map<string, string>;
 
+  @prop({ default: 'script' })
+  lambdaCodeSource?: string;
+
   @prop({ default: '' })
   lambdaRuntime?: string;
 
