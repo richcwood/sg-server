1890763676b6898792aae48c3713a176cba35e9f
Richard Wood <rich@wifunds.com>
2020-12-25 11:04:27 -0700

    filter agents by targetVersion

--- server/src/api/domain/Agent.ts
+++ server/src/api/domain/Agent.ts
@@ -86,7 +86,8 @@ export class AgentSchema {
         'createDate': [FilterOperator.EQUALS, FilterOperator.GREATER_THAN, FilterOperator.GREATER_THAN_EQUAL_TO, FilterOperator.LESS_THAN, FilterOperator.LESS_THAN_EQUAL_TO, FilterOperator],
         'tags': [FilterOperator.EQUALS],
         'offline': [FilterOperator.EQUALS],
-        'lastHeartbeatTime': [FilterOperator.LESS_THAN, FilterOperator.GREATER_THAN_EQUAL_TO]
+        'lastHeartbeatTime': [FilterOperator.LESS_THAN, FilterOperator.GREATER_THAN_EQUAL_TO],
+        'targetVerson': [FilterOperator.EQUALS]
         // 'dog.name': [FilterOperator.IN, FilterOperator.EQUALS, FilterOperator.NOT_EQUALS, FilterOperator.LIKE
         // ],
         // 'dog.smell': [FilterOperator.LIKE],
