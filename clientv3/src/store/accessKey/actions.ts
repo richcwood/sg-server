0df7bfaa0189d800f921335b8fa8f950996733cd
Bart  Wood <bartwood@barts-mbp-2.lan>
2021-02-19 18:38:49 -0700

    ignore bp for access key for create for myself

--- clientv3/src/store/accessKey/actions.ts
+++ clientv3/src/store/accessKey/actions.ts
@@ -2,6 +2,7 @@ import { ActionTree } from 'vuex';
 import { actions as coreActions } from '@/store/core/actions';
 import { CoreState, RootState, Model } from '@/store/types';
 import { AccessKey } from './types';
+import { stompInitialized, getStompHandler, DomainMessage, getCorrelationId } from '../../utils/StompHandler';
 
 export const actions: ActionTree<CoreState, RootState> = {  
   
@@ -32,5 +33,22 @@ export const actions: ActionTree<CoreState, RootState> = {
   updateSelectedCopy({commit, state}, updated: Model): Promise<Model|undefined> {
     return coreActions.updateSelectedCopy({commit, state}, updated);
   }
-  
 };
+
+
+// Custom stomp handlers usually go into a store's actions 
+stompInitialized.then(() => {
+  const stompHandler = getStompHandler();
+
+  // Keep the script name's in sync with any changes on the backend
+  stompHandler.registerMessageHandler('AccessKey', (message: DomainMessage) => {
+    // Only listen to AccessKey creation if the message did NOT get triggered by the active user
+    // Otherwise, the system will use the browser push access key IF the browser push message
+    // is recieved faster than the POST response.  And the accessKeySecret won't exist
+    if(    message.operation !== 1 || // create
+        (message.operation === 1 && getCorrelationId() !== message.correlationId)){
+      // Rely on the default handler for scripts
+      stompHandler.defaultMessageHandler(message);
+    }
+  });
+});
