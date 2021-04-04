5cf7a52f925adaa155d3eb2acacaf72506803325
Rich Wood <rich@saasglue.com>
2021-04-03 21:39:42 -0600

    moved agent downloader to a new project

--- BuildAgentDownloader.js
+++
@@ -1,5 +0,0 @@
-const pkg_1 = require("pkg");
-
-const pkg_path = './AgentDownloader'
-
-pkg_1.exec([`./AgentDownloader/download_sg_agent_win.js`, '--config', `${pkg_path}/package.json`, '--targets', 'node10-win-x64', '--out-path', `./${pkg_path}`]);
