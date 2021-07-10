7b379c5e332afe461be22d7f67fcad92ea051daf
Richard Wood <rich@saasglue.com>
2021-07-10 16:49:45 -0600

    updated logo

--- clientv3/src/App.vue
+++ clientv3/src/App.vue
@@ -15,7 +15,7 @@
     <div class="nav-bar" v-if="! isOnLandingPage()">
 
       <div class="left-nav">
-        <router-link class="logo-container" to="/"> <img src="/logo3.png" class="logo"> </router-link>
+        <router-link class="logo-container" to="/"> <img src="/logo4.png" class="logo"> </router-link>
         <router-link :class="{'active-link': isLinkActive(['downloadAgent'])}" class="main-nav-link" to="/downloadAgent">Download Agent</router-link><span class="nav-spacer">|</span>
         <router-link :class="{'active-link': isLinkActive(['jobList', 'jobDesigner'])}" class="main-nav-link" to="/jobList">Designer</router-link><span class="nav-spacer">|</span>
         <router-link :class="{'active-link': isLinkActive(['jobMonitor', 'jobDetailsMonitor'])}" class="main-nav-link" to="/jobMonitor">Monitor</router-link><span class="nav-spacer">|</span>
