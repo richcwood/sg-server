c9c3bacbc41129884978975bbaafe8d58b9db703
Richard Wood <rich@saasglue.com>
2022-06-16 22:00:53 -0600

    change accesskey _teamId property to string

--- server/src/api/domain/AccessKey.ts
+++ server/src/api/domain/AccessKey.ts
@@ -1,20 +1,19 @@
-import { modelOptions, prop, getModelForClass, Severity } from '@typegoose/typegoose';
-import { FilterOperator } from '../utils/BulkGet';
-import * as mongodb from 'mongodb';
-import { AccessKeyType } from '../../shared/Enums';
+import { modelOptions, prop, getModelForClass, Severity } from "@typegoose/typegoose";
+import { FilterOperator } from "../utils/BulkGet";
+import * as mongodb from "mongodb";
+import { AccessKeyType } from "../../shared/Enums";
 
 /// TODO: change some of these to required like ownerId and inviteLink
 // Example of a schema / domain in Mongoose
-@modelOptions({ schemaOptions: { collection: 'accessKey' }, options: { allowMixed: Severity.ALLOW } })
+@modelOptions({ schemaOptions: { collection: "accessKey" }, options: { allowMixed: Severity.ALLOW } })
 export class AccessKeySchema {
-
   _id?: mongodb.ObjectId;
 
   @prop()
   id?: mongodb.ObjectId;
-  
+
   @prop({ required: true })
-  _teamId: mongodb.ObjectId;
+  _teamId: string;
 
   @prop({ required: true })
   createdBy: mongodb.ObjectId;
@@ -45,13 +44,13 @@ export class AccessKeySchema {
 
   // Define which filters are legal for which props (including nested props (not sure about nested arrays))
   public static readonly validFilters = {
-    'accessKeyId': [FilterOperator.EQUALS, FilterOperator.IN]
+    accessKeyId: [FilterOperator.EQUALS, FilterOperator.IN],
   };
 
   // 2 way map between field values the API client sees and what is stored in the database.  Allows client to use 'id' and database to use '_id'
   public static readonly propAliases = {
-    '_id': 'id',
-    'id': '_id'
+    _id: "id",
+    id: "_id",
   };
 
   // Converters for values to/from the database.  Converter functions take the entire model
@@ -59,21 +58,20 @@ export class AccessKeySchema {
     // This isn't hooked up yet until needed - if it does, then call this in the controller layer on data before passing to service
     toDB: {
       _id: (data) => {
-        if(data && data._id){
+        if (data && data._id) {
           return new mongodb.ObjectId(data._id);
-        }
-        else {
+        } else {
           return undefined;
         }
-      }
+      },
     },
 
     fromDB: {
       accessKeySecret: (data) => {
         return undefined; // remove the version field - api users won't see it
-      }
-    } 
-  }
-};
+      },
+    },
+  };
+}
 
 export const AccessKeyModel = getModelForClass(AccessKeySchema);
