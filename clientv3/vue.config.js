5a2f03708636a70818d568c63d0d281d4faca3f9
Andrii Mozhlianskyi <mozhlyanskiy1993@gmail.com>
2022-10-29 22:52:47 +0300

    add yaml plugin

--- clientv3/vue.config.js
+++ clientv3/vue.config.js
@@ -2,7 +2,18 @@ const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
 module.exports = {
   configureWebpack: {
-    plugins: [new MonacoWebpackPlugin()],
+    plugins: [
+      new MonacoWebpackPlugin({
+        customLanguages: {
+          label: 'yaml',
+          entry: 'monaco-yaml',
+          worker: {
+            id: 'monaco-yaml/yamlWorker',
+            entry: 'monaco-yaml/yaml.worker',
+          },
+        },
+      })
+    ],
   },
   devServer: {
     proxy: "http://localhost:3000",
