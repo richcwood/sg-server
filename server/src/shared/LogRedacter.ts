9b6eecd4ac775529b7fc2a6d75188ee028dd957c
Richard Wood <rich@saasglue.com>
2023-04-25 22:32:36 -0600

    debug

--- server/src/shared/LogRedacter.ts
+++ server/src/shared/LogRedacter.ts
@@ -17,26 +17,33 @@ export class LogRedacter {
         // Redact sensitive information in the key value pairs
         if (!data) return data;
         const redact = (obj: LogData) => {
-            Object.entries(obj).forEach(([key, value]) => {
-                if (typeof value === 'string') {
-                    // Redact sensitive information based on the key
-                    if (this.redactList.some((pattern) => pattern.test(key))) {
-                        value = '[REDACTED]';
-                    } else {
-                        // Redact URI's with embedded credentials
-                        const matches = value.match(/(?<=:\/\/)(?<username>[^:]*):(?<password>[^@]*)@/);
-                        if (matches && matches.groups) {
-                            if (matches.groups.username) value = value.replace(matches.groups.username, '[REDACTED]');
-                            if (matches.groups.password) value = value.replace(matches.groups.password, '[REDACTED]');
+            try {
+                Object.entries(obj).forEach(([key, value]) => {
+                    if (typeof value === 'string') {
+                        // Redact sensitive information based on the key
+                        if (this.redactList.some((pattern) => pattern.test(key))) {
+                            value = '[REDACTED]';
+                        } else {
+                            // Redact URI's with embedded credentials
+                            const matches = value.match(/(?<=:\/\/)(?<username>[^:]*):(?<password>[^@]*)@/);
+                            if (matches && matches.groups) {
+                                if (matches.groups.username)
+                                    value = value.replace(matches.groups.username, '[REDACTED]');
+                                if (matches.groups.password)
+                                    value = value.replace(matches.groups.password, '[REDACTED]');
+                            }
                         }
-                    }
 
-                    obj[key] = value;
-                } else if (typeof value === 'object') {
-                    // Recursively redact sensitive information in the nested objects
-                    redact(value as LogData);
-                }
-            });
+                        obj[key] = value;
+                    } else if (typeof value === 'object') {
+                        // Recursively redact sensitive information in the nested objects
+                        redact(value as LogData);
+                    }
+                });
+            } catch (err) {
+                console.log('redactMessage - data ----------> ', data);
+                console.log('redactMessage - err ----------> ', err);
+            }
         };
 
         redact(data);
